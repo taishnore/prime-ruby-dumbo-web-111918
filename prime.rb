5ee08cb5f032806705ec5d4c77e7d317fc0c2ff2
Taimur Shah <tshah@scu.edu>
2018-11-11 23:56:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(n)
+  range = (2..n-1).to_a
+  counter = 0
+  if n < 2
+    false
+  elsif n >= 2
+    range.each do |number|
+      if n % number == 0
+        return false
+      end
+    end
+    true
+  end
+end
